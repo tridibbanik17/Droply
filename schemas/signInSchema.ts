1a2e2d6e0b7a0ba0d9c5d0cffeb2165be7a290ff
Tridib Banik <tridib.perfect@gmail.com>
2025-08-26 00:41:11 -0400

    Added Zod schemas

---
+++ schemas/signInSchema.ts
@@ -0,0 +1,12 @@
+import * as z from "zod"
+
+export const signInSchema = ({
+  identifier: z
+    .string()
+    .min(1, {message: "Email is required"})
+    .email({message: "Please enter a valid email"}),
+  password: z
+    .string()
+    .min(1, {message: "Password is required"})
+    .min(8, {message: "Passowrd must be at least 8 characters"}),
+});
---
+++ schemas/signUpSchema.ts
@@ -0,0 +1,21 @@
+import * as z from "zod";
+
+export const signUpSchema = z 
+  .object({
+    email: z
+      .string()
+      .min(1, { message: "Email is required" })
+      .email({ message: "Please enter a valid email" }),
+    password: z
+      .string()
+      .min(1, { message: "Password is required" })
+      .min(8, { message: "password should be minimum of 8 characters" }),
+    passwordConfirmation: z
+      .string()
+      .min(1, { message: "Please confirm your password" }),
+  })
+
+  .refine((data) => data.password === data.passwordConfirmation, {
+    message: "Password does not match",
+    path: ["passwordConfirmation"],
+  })
